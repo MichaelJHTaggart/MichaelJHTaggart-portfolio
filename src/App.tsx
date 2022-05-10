10d0a180638cd21e5ba593fef3d5eaab6e0b2212
Michael Taggart <mtaggarthawk@gmail.com>
2022-05-10 17:17:25 -0600

    feat(MP-6): mobile-view home component completed

--- src/App.tsx
+++ src/App.tsx
@@ -16,23 +16,22 @@ function App() {
   const dispatch = useDispatch();
   const { isOpen, isNew } = useSelector(selectNavbarState);
 
-//TODO: suspense to render the app once everything on the page is loaded.
+  //TODO: suspense to render the app once everything on the page is loaded.
 
   return (
     <div>
       <Navbar />
       {isOpen && <Menu slideLeft="animate-slide-in-left" />}
       {!isOpen && !isNew && <Menu slideLeft="animate-slide-out-left -z-50" />}
-        <Routes>
-          {/* <Route path="/login" element={<Login />} />
+      <Routes>
+        {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} /> */}
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/shopping" element={<Shopping />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/shopping" element={<Shopping />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/account" element={<Account />} />
         <Route path="/purchases" element={<Purchases />} /> */}
-        </Routes>
-      <h1 className="text-3xl font-bold">MichaelJHTaggart</h1>
+      </Routes>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,26 +5,37 @@ const Home = () => {
   const [isSpinable, setIsSpinable] = useState(false);
 
   return (
-    <div className="bg-slate-200 flex">
-      <img
-        className="relative right-20 bg-transparent "
-        src="/michael.png"
-        alt="michael"
-      />
-      <span className="relative break-all -top-28 -left-32 w-48 h-48 ">
-        {isSpinable ? (
-          <Spin />
-        ) : (
-          <div
-            onClick={() => setIsSpinable(true)}
-            className="hover:animate-pulse"
-          >
-            {/* <h1 className="font-[Cinzel] font-thin decoration-8 text-5xl">
-              Web Developer
-            </h1> */}
+    <div>
+      <div className="min-h-full flex-col items-center justify-center text-center">
+        <div className="min-h-full flex-col items-center justify-center text-center">
+          <div>
+            <div className="absolute -top-6 left-24 -z-40 bg-slate-300 w-36 h-36 rounded-full flex"></div>
+            <div className="absolute top-1 -left-10 -z-20 bg-sky-200 w-60 h-60 rounded-full flex"></div>
+            <div className="absolute top-36 -left-36 -z-30 bg-slate-300 w-56 h-56 rounded-full flex"></div>
+            <img
+              className="absolute right-16 bg-transparent"
+              src="/michael.png"
+              alt="michael"
+            />
+
+            <div className="absolute left-64 top-28 flex leading-normal text-left text-4xl drop-shadow-md font-thin h-48 w-38 break-all">
+              WEB <br />
+              DEVE- <br />
+              LOPER
+            </div>
           </div>
-        )}
-      </span>
+
+          <div className="relative top-72 flex-col">
+            <h3 className="text-3xl font-thin">MichaelJHTaggart</h3>
+            <button className="bg-blue-400 text-white rounded-lg w-10/12 h-14 mt-24 text-2xl">
+              Shop
+            </button>
+            <button className="bg-transparent rounded-lg w-10/12 h-14 mt-6 border-black border-2 text-2xl">
+              See Projects
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
